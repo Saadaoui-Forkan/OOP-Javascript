9d530633099b79a04a15da2bfccc96ce82afac53
unknown <forkan.saadaoui@gmail.com>
2024-08-26 15:41:00 +0100

    Class Static Properties And Methods

--- oop.js
+++ oop.js
@@ -1,22 +1,38 @@
 /*
   Class
+  Static Properties & Methods
 */
 
 class User {
-    constructor(name, email) {
-      this.name = name;
-      this.email = email;
-    }
-    sayHello() {
-      return `Hello ${this.name}`;
-    }
-    showEmail() {
-      return `Your Email Is ${this.email}`;
-    }
+  // Static Properties
+  static counter = 0;
+
+  constructor(name, email, counter) {
+    this.name = name;
+    this.email = email;
+    this.counter = counter;
+    User.counter++;
   }
-  
-  let user1 = new User("Osama", "o@nn.sa");
-  let user2 = new User("Ahmed");
-  
-  console.log(user1);
-  console.log(user2);
+  sayHello() {
+    return `Hello ${this.name}`;
+  }
+  showEmail() {
+    return `Your Email Is ${this.email}`;
+  }
+
+  // Static Methods
+  static countObjects = function () {
+    return `${this.counter} Objects Created.`;
+  };
+}
+
+let user1 = new User("Osama", "o@nn.sa", 2);
+let user2 = new User("Ahmed", "o@nn.sa", 2);
+let user3 = new User("Osama", "o@nn.sa", 2);
+let user4 = new User("Osama", "o@nn.sa", 2);
+// let user2 = User("Ahmed", "a@nn.sa"); // TypeError: Class constructor User
+
+console.log(typeof User); // Function
+console.log(User === User.prototype.constructor); // True
+
+console.log(User.countObjects());
